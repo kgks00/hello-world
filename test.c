c0ddd67dbce5d51face5fecafbe3cd10023281a3
kgks00 <54614914+kgks00@users.noreply.github.com>
2019-08-28 14:31:36 +0900

    Add files via upload

---
+++ test.c
@@ -0,0 +1,11 @@
+scans will cause conflict with the local TCP/IP stack. This means you need to
+either use the `-S` option to use a separate IP address, or configure your
+operating system to firewall the ports that masscan uses.
+
+This tool is free, but consider funding it here:
+1MASSCANaHUiyTtR3bJ2sLGuMw5kDBaj4T
+
+
+# Building
+
+On Debian/Ubuntu, it goes something like this:
